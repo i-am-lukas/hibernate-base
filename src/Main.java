53d8386a2c0ebca052a30a923a22ebd4e5d5d383
lukas <callmeluke@live.com>
2015-05-05 09:37:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Main {
+
+    public static void main(String[] args9) {
+
+        SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        session.beginTransaction();
+        session.getTransaction().commit();
+
+        session.close();
+        factory.close();
+    }
+
+}
