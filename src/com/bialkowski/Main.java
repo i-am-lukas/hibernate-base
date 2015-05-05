ebce21f57d86c7cabc83381001baa4913e6ed34e
lukas <callmeluke@live.com>
2015-05-05 10:13:25 +0200

    OneToOne - one directional

--- src/com/bialkowski/Main.java
+++ src/com/bialkowski/Main.java
@@ -1,3 +1,5 @@
+package com.bialkowski;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -9,7 +11,17 @@ public class Main {
         SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
         Session session = factory.openSession();
 
+        Address address = new Address();
+        address.setUlica("Kasprowicza");
+        address.setNrMieszkania(21);
+
+        Employee employee = new Employee();
+        employee.setImie("Darek");
+        employee.setNazwisko("Kwiatek");
+
         session.beginTransaction();
+        session.save(address);
+        session.save(employee);
         session.getTransaction().commit();
 
         session.close();
