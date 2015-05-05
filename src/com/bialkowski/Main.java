70691383112c327cddd6cdb0dcc5fc0b82179c06
lukas <callmeluke@live.com>
2015-05-05 11:25:02 +0200

    OneToOne - two directional

--- src/com/bialkowski/Main.java
+++ src/com/bialkowski/Main.java
@@ -6,6 +6,8 @@ import org.hibernate.cfg.AnnotationConfiguration;
 
 public class Main {
 
+//    nie musze dodawac obydwu obiektow. Hibernate sie tym zajmie za mnie
+
     public static void main(String[] args9) {
 
         SessionFactory factory = new AnnotationConfiguration().configure().buildSessionFactory();
@@ -19,9 +21,18 @@ public class Main {
         employee.setImie("Darek");
         employee.setNazwisko("Kwiatek");
 
+        employee.setAddress(address);
+
+        Employee employee2 = new Employee();
+        employee2.setImie("Darek");
+        employee2.setNazwisko("Kwiatek");
+
+        employee2.setAddress(address);
+
         session.beginTransaction();
-        session.save(address);
+//        session.save(address);
         session.save(employee);
+        session.save(employee2);
         session.getTransaction().commit();
 
         session.close();
